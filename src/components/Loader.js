43bd187b84e703d8231a180f2b577d93445330d5
btuchowski <btuchowski@fream.pl>
2019-03-15 11:53:24 +0100

    added Loader & searching functionality

---
+++ src/components/Loader.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import injectSheet from 'react-jss';
+
+const styles = {
+    loader: {
+        color: 'blueviolet'
+    }
+}
+
+const Progress = ({classes}) => <CircularProgress className={classes.loader}/>
+
+const StyledProgress = injectSheet(styles)(Progress)
+
+function Loader() {
+    return <StyledProgress />
+}
+
+export default Loader;
