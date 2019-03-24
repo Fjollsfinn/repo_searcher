1bbe0a5a1844d12dff1246fc662d95e30fb04ae3
Fjollsfinn <btuchowski@fream.pl>
2019-03-24 17:20:08 +0100

    Updates

---
+++ precache-manifest.4973a66e97fc65c16aae2895ca3f690c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5fdce0d5d353706c2e2",
+    "url": "/repo_searcher/static/js/runtime~main.09daff8c.js"
+  },
+  {
+    "revision": "4843d940c16f3c800dad",
+    "url": "/repo_searcher/static/js/main.2020ca00.chunk.js"
+  },
+  {
+    "revision": "7487563b88ec239c331a",
+    "url": "/repo_searcher/static/js/2.b83ca5fa.chunk.js"
+  },
+  {
+    "revision": "4843d940c16f3c800dad",
+    "url": "/repo_searcher/static/css/main.fa820814.chunk.css"
+  },
+  {
+    "revision": "3e2f141dc244fb0d826cc73da5c493b3",
+    "url": "/repo_searcher/index.html"
+  }
+];
